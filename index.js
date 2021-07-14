67f4baeb0ea25b384834736c92243fe653488400
Ryochi <ryo1.t.zubu@gmail.com>
2021-07-14 21:14:28 +1000

    Done with assingment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, string){
+    let matches = drivers.filter(name => {
+        if (name.toUpperCase() == string.toUpperCase()){
+            return name;
+        }
+    })
+    return matches;
+}
+
+function fuzzyMatch(drivers, string){
+    let matches = drivers.filter(name => {
+        if(name.charAt(0).toUpperCase() == string.charAt(0).toUpperCase()){
+            return name;
+        }
+    });
+    return matches;
+}
+
+function matchName(drivers, string){
+    return drivers.filter(key => key.name == string );
+}
